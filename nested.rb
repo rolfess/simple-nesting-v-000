a74e73d3aa923edba5a2969b7e19dc4ed84431a6
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-23 03:52:07 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -74,10 +74,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-		 programmer_hash [:yukihiro_matsumoto] = {
-			:known_for => "Ruby",
-			:languages => ["LISP", "C"]
-			}
+		 programmer_hash [:yukihiro_matsumot] = {
+		 :known_for => "Ruby",
+		 :languages => ["LISP", "C"]
+		 }
 
 end
 
