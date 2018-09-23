d27e86653277bc9e33b918c8c5ef6c013e351cce
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-23 03:30:41 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 drlanguage.inspect = programmer_hash [:dennis_ritchie][:languages]
+		 drlanguage = programmer_hash [:dennis_ritchie][:languages]
 
     end
 
